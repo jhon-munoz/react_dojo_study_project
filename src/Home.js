45826ad422acfdd018e6f1df304ae2a2f5db1d69
Jhon Munoz <jhon.munoz@teaminternational.com>
2024-04-28 09:20:58 -0500

    Functions as props

--- src/Home.js
+++ src/Home.js
@@ -13,9 +13,14 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="Home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 };
