3633a92fd9ab73bb20706598a53858b47116d5ec
Jhon Munoz <jhon.munoz@teaminternational.com>
2024-04-28 07:03:24 -0500

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello Ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
+
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Jhon", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
